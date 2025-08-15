16f7b1c5cc840302d24d0ba012e0ed3a065c3032
Eder Barrios <eder_barrios@epam.com>
2025-08-15 11:52:52 -0500

    Add routes for surveys, questions and mass operations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\SurveyController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Survey routes
+Route::resource('surveys', SurveyController::class);
+
+// Question routes
+Route::resource('questions', QuestionController::class);
+
+// Mass update routes for questions
+Route::post('questions/mass-assign', [QuestionController::class, 'massAssign'])->name('questions.mass-assign');
+Route::delete('questions/mass-delete', [QuestionController::class, 'massDelete'])->name('questions.mass-delete');
